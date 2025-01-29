a285de98abfa376a3f964a19976b742e07872a10
Lcordero21 <lcordero@willamette.edu>
2025-01-28 20:27:40 -0800

    commit and push

---
+++ projects/Bags/bag.py
@@ -0,0 +1,37 @@
+from typing import Iterable, Optional
+from datastructures.ibag import IBag, T
+
+
+class Bag(IBag[T]):
+    def __init__(self, *items: Optional[Iterable[T]]) -> None:
+        self._bag: dict[T, int] = {}
+
+        if items is not None:
+            for item in items:
+                self.add(item)
+
+    def add(self, item: T) -> None:
+        if item in self._bags:
+            self._bag[item] += 1
+        else:
+            self._bag[item] = 1
+        if item is None:
+            raise TypeError
+
+    def remove(self, item: T) -> None:
+        raise NotImplementedError("remove method not implemented")
+
+    def count(self, item: T) -> int:
+        return self._bag[item]
+
+    def __len__(self) -> int:
+        raise NotImplementedError("__len__ method not implemented")
+
+    def distinct_items(self) -> Iterable[T]:
+        raise NotImplementedError("distinct_items method not implemented")
+
+    def __contains__(self, item) -> bool:
+        raise NotImplementedError("__contains__ method not implemented")
+
+    def clear(self) -> None:
+        raise NotImplementedError("clear method not implemented")
