0ec94af95ac03d0c42a7ff2adf1ca68450b78927
Lcordero21 <lcordero@willamette.edu>
2025-02-02 19:50:28 -0800

    ermm idk

---
+++ projects/project1/main.py
@@ -0,0 +1,3 @@
+import program
+
+player=Character()
--- projects/project1/program.py
+++ projects/project1/program.py
@@ -1,2 +1,10 @@
 import random
+import bag
 
+class Character:
+    name: str
+    score: int
+    attack_power: int
+
+class Game:
+    def __init__(self,player:Character, dealer:Character):
