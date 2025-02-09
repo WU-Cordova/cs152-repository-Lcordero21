3f67847eab0ffdc5a1bf489fc79ad334e56de9e1
Lcordero21 <lcordero@willamette.edu>
2025-02-09 13:07:00 -0800

    Legit a start

--- projects/project1/card.py
+++ projects/project1/card.py
@@ -1,17 +1,16 @@
+import copy
+from dataclasses import dataclass
 from enum import Enum
-from datastructures.bag import Bag
-
-
-bag = Bag()
+import random
 
 class CardSuit(Enum):
-    HEARTS = ""
-    SPADE = ""
-    CLUBS = ""
-    DIAMONDS = ""
+    HEARTS = "♥"
+    SPADE = "♠"
+    CLUBS = "♣"
+    DIAMONDS = "♦"
 
 class CardFace(Enum):
-    ONE = "1"
+    ACE = "11" #remember ace is a special case where it can also equal 1 if it causes a bust
     TWO = "2"
     THREE = "3"
     FOUR = "4"
@@ -20,13 +19,28 @@ class CardFace(Enum):
     SEVEN = "7"
     EIGHT= "8"
     NINE = "9"
-    JACK="10"
-    QUEEN="10"
-    KING = "10"
-    ACE="1" #keep in mind an ace can also be an eleven if paired with a face card
+    TEN = "10"
+    JACK="J"
+    QUEEN="Q"
+    KING = "K"
+    def face_value(self) -> int:
+        match self:
+            case CardFace.JACK | CardFace.QUEEN | CardFace.KING:
+                return 10
+            case CardFace.ACE:
+                return 11
+            case _:
+                return int(self.value)
+    
     
 
-
+@dataclass
 class Card:
     card_face: CardFace
     card_suit: CardSuit
+
+    def __hash__(self) -> int:
+        return hash (self.card_face.name) * hash(self.card_suit.name)
+    def __str__(self) -> str:
+        return f"[{self.card_face.value}{self.card_suit.value}]"
+    
--- projects/project1/program.py
+++ projects/project1/program.py
@@ -1,17 +1,30 @@
+import copy
 import random
 from datastructures.bag import Bag
-from project1.card import Card, CardSuit, CardFace
+from projects.project1.card import Card, CardSuit, CardFace
 
 def main():
-    card_suits = [suit.value for suit in list(CardSuit)]
-    cards = []
-    for suit in list(CardSuit):
-        for face in list(CardFace):
-            cards.append(Card(suit.value, face.value))
+    one_deck_list = [Card(face, suit) for suit in CardSuit for face in CardFace]
+    print(f"One deck has {len(one_deck_list)} cards")
+    print("".join(str(card) for card in one_deck_list))
 
-    print(cards)
+    deck_count = random.choice([2, 4, 6, 8])
+    multi_deck_list = [card for _ in range(deck_count) for card in copy.deepcopy(one_deck_list)]
 
+    print(f"{deck_count} decks have {len(multi_deck_list)} cards")
+    print("".join(str(card) for card in multi_deck_list))
 
+    deck_bag = Bag(*multi_deck_list)
+    print(list(deck_bag.distinct_items()))
+    two_cards = random.sample(list(deck_bag.distinct_items()), 2)
+    print(f"Two cards: {"".join(str(card) for card in two_cards)} with a face value of: {sum(card.face.face_value() for card in two_cards)}")
+
+if __name__ == '__main__':
+    main()
+
+"""import random
+from datastructures.bag import Bag
+from projects.project1.card import Card, CardSuit, CardFace
 
 class Game:
     def __init__(self,player, dealer):
@@ -19,7 +32,21 @@ class Game:
         self.__dealerScore__=0
         self.__player__=player
         self.__dealer__=dealer
-        main()
-    
-    def shuffleCards(self):
-        numOfDecks= 1
+
+
+    def main():
+        number=random.randint(2,8)
+        card_suits = [suit.value for suit in list(CardSuit)]
+        print(card_suits)
+        cards = []
+        for i in range(2):
+            for face in list(CardSuit):
+                for suit in list(CardFace):
+                    cards.append(suit)
+        print(cards)
+        print(len(cards))
+    main()
+
+if __name__ == '__main__':
+    Game"""
+
