efb94949cbcce6584d3418aa513e67e2516ed6bb
Lcordero21 <lcordero@willamette.edu>
2025-03-05 12:56:49 -0800

    even more!!

--- projects/project2/__init__.py
+++ projects/project2/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from collections.abc import Sequence
 import os
-from typing import Any, Iterator, overload
+from typing import Any, Iterator, List, overload
 import numpy as np
 from numpy.typing import NDArray
 import copy
@@ -18,6 +18,7 @@ class Cell:
         self.row_index = row_index
         self.col_index = col_index
         self.status = status
+
 
     def next_state(self, num_neighbors):
         if num_neighbors < 2:
@@ -52,6 +53,9 @@ class Grid:
             for col in range (cols):
                 self.grid[row][col].is_alive = random.choice([True,False])
 
+    def get_neighbour(self):
+        raise NotImplementedError
+
     def display(self):
         for row in range(self.rows):
             for col in range(self.cols):
@@ -61,7 +65,12 @@ class Grid:
 
 
     def nextGeneration (self):
-        newGen = NDArray() #make empty array
+        newGen = Grid(self.rows,self.cols) #make empty array
+        for row in range(self.rows):
+            for col in range (self.cols):
+                num_neighbours = self.get_neighbour(row,col) #implement checking for neighbours here
+                next_state = self.grid[row][col].next_state(num_neighbours) #implement rules here
+                next_grid[row][col].is_alive = next_state #double check
 
     def __eq__(self,value):
         pass
@@ -72,6 +81,7 @@ class GameController:
     #Add all key functions here
     def __init__(self, grid:Grid):
         self.grid = grid
+        self.history: List[Grid] = []
 
     def run(self):
         self.grid.display()
@@ -81,12 +91,28 @@ class GameController:
         kbhit = KBHit()
 
         while True:
+            self.grid.display()
             sleep(1)
             if kbhit.kbhit():
                 key = kbhit.getch()
 
                 if key == 'q':
                     print("you hit quit")
-                    return                    
+                    return     
+
+
+
+                           
         #Print all Instructions here Ex: [print("Press 'q' to quit.")]
         #account for uppercase and lowercase
+        #break will end the loop
+
+
+        """
+        1.Print Grid
+        2. loop until break
+        3. generate next grid (itereate grid, count neighbours, run rules)
+        4.Save current grid to grid history
+        5.print grid
+        6. set grid = new_grid
+        """
