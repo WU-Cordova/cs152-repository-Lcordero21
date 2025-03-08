fa1bb1168b7d7eab86abb5b35c9a67c144a88b25
Lcordero21 <lcordero@willamette.edu>
2025-03-07 22:39:17 -0800

    i messed it all up...

--- projects/project2/__init__.py
+++ projects/project2/__init__.py
@@ -19,11 +19,11 @@ class Cell:
 
     def next_state(self, num_neighbors):
         if num_neighbors < 2:
-            self.status = 0
+            self.status = False
         elif num_neighbors == 3:
-            self.status = 1
+            self.status = True
         elif num_neighbors >= 4:
-            self.status = 0
+            self.status = False
         else:
             pass
         return self.status
@@ -49,7 +49,7 @@ class Grid:
     def __init__ (self, rows, cols):
         self.num_rows = rows
         self.num_cols = cols
-        self.grid: Array2D = Array2D.empty (rows,cols, data_type=Cell)
+        self.grid: Array2D[Cell] = Array2D.empty (rows,cols, data_type=Cell)
 
         for row in range(rows):
             for col in range (cols):
@@ -59,17 +59,17 @@ class Grid:
         self.neighbours=0
         if row_index != (self.num_rows-1): 
             self.neighbours+=self.notInFirstOrLastCol(row_index,col_index)
-            if self.grid[row_index+1][col_index].status == 1:
+            if self.grid[row_index+1][col_index].status == True:
                 self.neighbours+=1
             if col_index != (0):
-                if self.grid[row_index+1][col_index-1].status == 1:
+                if self.grid[row_index+1][col_index-1].status == True:
                     self.neighbours+=1
             if col_index != (self.num_cols-1):
-                if self.grid[row_index+1][col_index+1].status == 1:
+                if self.grid[row_index+1][col_index+1].status == True:
                     self.neighbours+=1
-        if row_index != "0":
+        if row_index != 0:
             self.neighbours+=self.notInFirstOrLastCol(row_index,col_index)
-            if self.grid[row_index-1][col_index].status == 1:
+            if self.grid[row_index-1][col_index].status == True:
                 self.neighbours+=1
             if col_index != (self.num_cols-1):
                 if self.grid[row_index-1][col_index+1].status == 1:
@@ -83,10 +83,10 @@ class Grid:
 
     def notInFirstOrLastCol(self, row_index, col_index): #a local function I created so I don't have to copy and paste this a ton in my if statement.
         neighboursToo = 0
-        if col_index == (self.num_cols -1): 
+        if col_index != 0: 
             if self.grid[row_index][col_index-1].status == 1:
                 neighboursToo+=1
-        if col_index == "0":
+        if col_index != (self.num_cols-1):
             if self.grid[row_index][col_index+1].status == 1:
                 neighboursToo+=1
         return neighboursToo
@@ -100,72 +100,121 @@ class Grid:
 
 
     def nextGeneration (self):
-        next_grid = Array2D.empty(self.num_rows,self.num_cols, data_type=Cell) #make empty array
+        next_grid = Array2D[Cell].empty(self.num_rows,self.num_cols, data_type=Cell) 
         for row in range(self.num_rows):
             for col in range (self.num_cols):
-                num_neighbours = self.get_neighbour(row,col) #implement checking for neighbours here
-                next_state = self.grid[row][col].next_state(num_neighbours) #implement rules here
-                next_grid[row][col].status = next_state #double check
-                print(num_neighbours, next_state,next_grid[row][col])
-
+                num_neighbours = self.get_neighbour(row,col) 
+                next_state = self.grid[row][col].next_state(num_neighbours) 
+                next_grid[row][col].status = next_state
         return next_grid
+
+        """for row in range(self.num_rows):
+            for col in range (self.num_cols):
+                self.grid[row][col].status = self.next_grid[row][col].status"""
+
+
+    def generationEquality(self, generation,history):
+        self.history = history
+        stability = True
+        if self.grid != self.history[generation]:
+            stability = False
+        """for row in range(self.num_rows):
+            for col in range(self.num_cols):
+                if self.grid[row][col].status != self.history[generation][row][col].status:
+                    stability = False """
+        return stability
 
     def __eq__(self,value):
         if isinstance(value,Grid) and self.num_rows == value.num_rows and self.num_cols == value.num_cols:
             return self.grid == value.grid
 
 class GameController:
-
-    #Add all key functions here
     def __init__(self, grid:Grid):
         self.grid = grid
         self.history: List[Grid] = []
 
     def run(self):      
-        generation = 0  
-        mode= "automatic????" #add user input asking what mode they want
-        speed="TBD" #add user input asking what speed they want (if they chose automatic)
-        print("Press 'q' to quit, 'd' to move a generation forward, 'a' to move a generation backwards, \n 'r' to automatically run through the generations, and 's' to start manually going through the generations again!")
+        generation = 0 
+
+
+        """print("Welcome to Conway's Game of Life!🦠")
+        while True:
+            if input("What mode would you like? Press '1' for Automatic or '2' for Manual:") == "1":
+                self.mode = "A"
+                try:
+                    self.speed = int(input("What speed in seconds would you like for the program to run?"))
+                except ValueError:
+                    print("Sorry, I don't understand that...")
+                else:
+                    print("Press 'Q' to stop the program at any time")
+                    break
+            if input("What mode would you like? Press '1' for Automatic or '2' for Manual:") == "2":
+                self.mode = "M"
+                print("Press 'D' to move forward a generation and press 'Q' to quit the program at any time. "
+                "The program will automatically end once the colony is stable.")
+                break
+            else:
+                print("Please input a valid number.")""" #Add this back in when fixed
 
         print("You are on Generation:", generation)
-        self.grid.display()
-        self.history.append(self.grid)
+        self.grid.display()        
         kbhit = KBHit()
-
         while True:
-            if kbhit.kbhit():
-                key = kbhit.getch()
-                stability = False
 
-                if key == 'd' or key == 'D':
-                    if stability == False:
-                        if (generation + 1)>len(self.history)-1:
-                            self.grid = self.grid.nextGeneration()
-                            self.history.append(self.grid)
-                            generation+=1
-                            print("You are on Generation:", generation)
-                            print(self.grid)
-                if key == 'r' or key == 'R': #it needs to start running from wherever user leaves off on manual
+            self.history.append(self.grid)
+            """if self.mode == "M":
+                if kbhit.kbhit():
+                    key = kbhit.getch()
+                    if key == 'd' or key == 'D':
+                        self.grid.nextGeneration()
+                        self.history.append(self.grid)
+                        generation+=1
+                        print("You are on Generation:", generation)
+                        self.grid.display()
+                        """"""if len(self.history)>1:
+                            stability = self.grid.generationEquality(generation-1,self.history)
+                            if stability == True:
+                                print("Colony is stable. Terminating simulation.")
+                                quit()
+                        if len (self.history)>2:
+                            stability = True
+                            stability = self.grid.generationEquality(generation-2,self.history)
+                            if stability == True:
+                                print ("Colony is stable. Terminating simulation.")
+                                quit()""""""
+
+                    if key == 'q' or key == 'Q':
+                        print("You hit quit")
+                        return
+
+                if self.mode == "A":
                     while True:
-                        sleep(1)
-                        if (generation + 1) < len(self.history-1):
-                            self.grid = self.grid.nextGeneration()
-                            self.history.append(self.grid)
-                            generation+=1
-                if key == 'q' or key == 'Q':
-                    print("You hit quit")
-                    return
-            
-    def checkGridStability (self, index): #DOUBLE CHECK!!!
-            stability = True
-            for row in range(self.num_rows):
-                for col in range(self.num_cols):
-                    if self.grid[row][col].status != self.history[index][row][col].status:
-                        stability = False    
-        #START OUT WITH ASKING PLAYER WHAT SPEED THEY WANT AND WHAT MODE!!!!
+                        sleep(self.speed)
+                        self.grid.nextGeneration()
+                        self.history.append(self.grid)
+                        generation+=1
+                        print("You are on Generation:", generation)
+                        self.grid.display()                    
+                        if kbhit.kbhit():
+                            key = kbhit.getch()
+                            if key == 'q' or key == 'Q':
+                                print("You hit quit")
+                                return
+                        """"""if len(self.history)>1:
+                            stability = self.grid.generationEquality(generation-1,self.history)
+                            if stability == True:
+                                print("Colony is stable. Terminating simulation.")
+                                return
+                        if len (self.history)>2:
+                            stability = self.grid.generationEquality(generation-2,self.history)
+                            if stability == True:
+                                print ("Colony is stable. Terminating simulation.")
+                                return"""
+            self.grid = self.grid.nextGeneration()
+            self.history.append(self.grid)
+            generation+=1
+            self.grid.display()
 
-        #account for uppercase and lowercase
-        #break will end the loop
 
 
     """ 1.Print Grid
