c5c6b2e8f218128d8c8b41d41a8f79ae7047f430
Lcordero21 <lcordero@willamette.edu>
2025-04-25 16:36:37 -0700

    More stuff

--- projects/project3/program.py
+++ projects/project3/program.py
@@ -1,8 +1,13 @@
 
 
 def main():
-    
-    print("Hello, World!")
+    def is_prime (n):
+        while True:
+            num = n*2
+            for i in range (2,(num ** 0.5)):
+                if (num % i) == 0:
+                    num+=1
+                    
 
 
 
--- projects/project4/program.py
+++ projects/project4/program.py
@@ -1,9 +1,14 @@
 
 
 def main():
-    
-    print("Hello, World!")
+    Col1=["MATH251", "CS152", "DATA331"]
+    Col2 = ["MATH251"]
+    majorReq = []
 
+    if isinstance (Col1, Col2):
+        majorReq.append()
+
+    print(majorReq)
 
 
 if __name__ == '__main__':
